086fa8a65517f16bc94134cd8877606865683b71
ASHFAQ AFZAL CHOWDHURY <aacfahim@gmail.com>
2020-08-10 13:12:36 +0600

    ver 2

--- logcheck.php
+++ logcheck.php
@@ -1,75 +1,72 @@
 <?php
-
+    
     include("connection.php");
     session_start();
+	
+	if(isset($_POST['submit'])){
 
-    if(isset($_POST['submit'])){
-
-        $userName = $_POST['userName'];
-        $password = $_POST['password'];
-
-        if(empty($userName) || empty($password)){
-            echo "Username/Password is needed";
-        }
-        else{
-
-            $sql = "select * from userLogin where id='".$username."' and password='".$password."'";
+		$username = $_POST['userName'];
+		$password = $_POST['password'];
+		
+		if($username == "" || $password == "" ){
+            header('location: login.html?msg=All fields are required');
+		}else{
+			
+			$sql = "select * from userLogin where id='".$username."' and password='".$password."'";
 
 
 			$result = mysqli_query($connection, $sql);
 			$row = mysqli_fetch_assoc($result);
 
 
-            if(count($row) > 0){
+			if(count($row) > 0){
 
+                $userName = $row['ID'];
+                $name = $row['Name'];
+                $email = $row['Email'];
+                $userType = $row['Type'];
+
+                //echo  $userType;
+
+                setcookie('STATUS', 'OK', time()+3600, '/');
+                //echo "succeeded";
+				//setcookie('password', $password, time()+3600, '/');
+
+				// storing name and emails from database to cookie
+				setcookie('name', $name, time()+3600, '/');
+                setcookie('email', $email, time()+3600, '/');
                 
+               // echo $userType;
 
-                    setcookie('STATUS', 'OK', time()+3600, '/');
-                    header('location: dashboard.php');
+                if($userType == "user")
+                {
+                     $_SESSION['status'] = "OK";
+                     //echo "OK";
+				     header('location: UserHome.html');
 
-                    $file  = fopen('user.txt', 'r');
-			        $data  = fread($file, filesize('user.txt'));
-                    fclose($data);
-                    
-                    $Adminfile  = fopen('admin.txt', 'r');
-			        $Admindata  = fread($Adminfile, filesize('admin.txt'));
-			        fclose($Admindata);
-
-                    $user = explode('|', $data);
-                    $admin = explode('|', $Admindata);
-
-                    if($userName == trim($user[0]) && $password == trim($user[1])){
-				        $_SESSION['status'] = "OK";
-				        header('location: UserHome.html');
-                    }
-                    else if($userName == trim($admin[0]) && $password == trim($admin[1]))
-                    {
-                        $_SESSION['status'] = "OK";
-                        header('location: AdminHome.html');
-
-                    }
-                else{
-				    header('location: login.php?msg=invalid_username/password');
-			    }
-
-
-            }else{
-                    echo "Wrong credentials";
-                    //header('location:login.html');
                 }
-            }
-            else{
-                echo "Account doesn't exist";
-            }
-            
-        }
+                if($userType == "admin")
+                {
+                    $_SESSION['status'] = "OK";
+                    header('location: AdminHome.html');
 
-    }
-    else{
-        //echo "invalid request";
-        header('location:login.php'); 
+                }
 
-    }
+				//echo "WELCOME";
+				
+				
+			}else{
+                echo " Invalid username or password";
+               // header('location: login.html?msg=invalid username or password');
+			}
+		}	
+
+	}else{
+		echo "invalid request";
+		
+	}
+
+
 
 
 ?>
